fcf7aa1d0cba8e38d7e677d43fb0378e4324bfab
TShipley <taneymail@gmail.com>
2020-02-01 20:54:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,1 +1,9 @@
 def oxford_comma(array)
+
+  if array.size == 1 
+    return array.first
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    return array [0..-2].join(', ') + ", and " + array.last
+  end
